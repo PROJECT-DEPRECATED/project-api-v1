ee0b414b025e631b25bdee071e550b030597ddb7
Project_TL <me@projecttl.net>
2022-12-20 08:15:22 +0900

    fix: database utils

--- utils/account.go
+++ utils/account.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 
-	"github.com/devproje/project-website/config"
 	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
 )
@@ -18,9 +17,7 @@ type Account struct {
 
 func (a *Account) isExist() bool {
 	var data Account
-
-	conf, _ := config.Get()
-	coll := DB.Database(conf.Database.DbName).Collection("account")
+	coll := DB.Collection("account")
 	err := coll.FindOne(context.TODO(), bson.D{{Key: "email", Value: a.Email}}).Decode(&data)
 
 	return err == nil
@@ -30,8 +27,7 @@ func (a *Account) New() error {
 	if a.isExist() {
 		return errors.New("email already exist")
 	}
-	conf, _ := config.Get()
-	coll := DB.Database(conf.Database.DbName).Collection("account")
+	coll := DB.Collection("account")
 	_, err := coll.InsertOne(context.TODO(), bson.D{
 		{Key: "_id", Value: uuid.NewString()},
 		{Key: "name", Value: a.Name},
@@ -49,8 +45,7 @@ func (a *Account) Drop() error {
 	if !a.isExist() {
 		return errors.New("email not exist")
 	}
-	conf, _ := config.Get()
-	coll := DB.Database(conf.Database.DbName).Collection("account")
+	coll := DB.Collection("account")
 	_, err := coll.DeleteOne(context.TODO(), bson.D{{Key: "email", Value: a.Email}})
 	if err != nil {
 		return err
@@ -61,9 +56,7 @@ func (a *Account) Drop() error {
 
 func (a *Account) Find() (*Account, error) {
 	var data Account
-
-	conf, _ := config.Get()
-	coll := DB.Database(conf.Database.DbName).Collection("account")
+	coll := DB.Collection("account")
 	err := coll.FindOne(context.TODO(), bson.D{{Key: "name", Value: a.Name}}).Decode(&data)
 	if err != nil {
 		return nil, err
--- utils/data.go
+++ utils/data.go
@@ -10,26 +10,28 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var DB *mongo.Client
+var DB *mongo.Database
 
 func Connect() error {
 	var err error
+	var client *mongo.Client
 	conf, _ := config.Get()
 	data := conf.Database
 
-	uri := fmt.Sprintf("mongodb://%s:%d/%s", data.URL, data.Port, data.DbName)
+	uri := fmt.Sprintf("mongodb://%s:%d", data.URL, data.Port)
 	option := options.Client().ApplyURI(uri)
 	option.SetAuth(options.Credential{
 		AuthMechanism: "SCRAM-SHA-256",
 		Username:      data.Username,
 		Password:      data.Password,
 	})
-	DB, err = mongo.Connect(context.TODO(), option)
+	client, err = mongo.Connect(context.TODO(), option)
 	if err != nil {
 		log.Errorln("Database connection failed.")
 		return err
 	}
 
+	DB = client.Database(data.DbName)
 	log.Infoln("Database connected.")
 	return nil
 }
